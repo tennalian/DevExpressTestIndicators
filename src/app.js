f6ae674f0ef4ffb8f8b61c0e67da1da240182a04
Мария Гнитецкая <m.gniteckaja@systtech.ru>
2018-09-17 16:31:45 +0200

    update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 import 'devextreme/dist/js/dx.all.js';
 
+import './styles.less';
+
 const init = class StData {
   constructor(id, data) {
     this.id = id;
@@ -9,8 +11,6 @@ const init = class StData {
   }
 
   chart() {
-    console.log(this.id);
-    console.log($(`#${this.id}`));
     $(`#${this.id}`).dxChart({
       dataSource: DevExpress.data.query(this.data).sortBy("count", true).toArray(),
       series: {
@@ -19,7 +19,43 @@ const init = class StData {
       }
     });
   }
+
+  progress(val) {
+    const template = `<div id="st-progress-dashboard">
+        <div data-options="dxTemplate: { name: 'content' }">
+            <div id="toolbar"></div>
+            <div class="progress"></div>
+        </div>
+    </div>`;
+    // $(`#${this.id}`).dxProgressBar({
+    //   height: 10,
+    //   readOnly: true,
+    //   showStatus: false,
+    //   value: val,
+    //   elementAttr: {
+    //     class: `st-progress ${val < 100 ? 'st-progress-red' : 'st-progress-green'}`
+    //   }
+    // });
+
+    $(`#${this.id}`).innerHtml = template;
+
+    const slideOutView = $(`#${this.id}`).dxSlideOutView({
+        contentTemplate: 'content'
+    }).dxSlideOutView("instance");
+
+    const progress = $(`#${this.id} .progress`).dxProgressBar({
+      height: 10,
+      readOnly: true,
+      showStatus: false,
+      value: val,
+      elementAttr: {
+        class: `st-progress ${val < 100 ? 'st-progress-red' : 'st-progress-green'}`
+      }
+    }).dxProgressBar("instance");
+  }
 }
+
+// https://js.devexpress.com/Documentation/Guide/Widgets/SlideOutView/Customize_the_View/
 
 
 export { init };
